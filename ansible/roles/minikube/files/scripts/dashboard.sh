8d9529cb790b2383c6a99fde7367fd3700251127
Ara <aram.airapetian@gmail.com>
2026-09-14 19:33:26 +0000

    fix: resolve minikube deployment issue

--- ansible/roles/minikube/files/scripts/dashboard.sh
+++ ansible/roles/minikube/files/scripts/dashboard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # IP-address of the host machine
-ipaddress=$(ip addr | grep eth0 | tail -n 1 | awk '{print $2}' | cut -d/ -f1)
+ipaddress=$(ip addr | grep eth0 | grep 192.168 | awk '{print $2}' | cut -d/ -f1)
 
 # Port forward the Kubernetes dashboard service to the host machine
 minikube kubectl -- -n kubernetes-dashboard port-forward --address "$ipaddress" svc/kubernetes-dashboard 8001:80
