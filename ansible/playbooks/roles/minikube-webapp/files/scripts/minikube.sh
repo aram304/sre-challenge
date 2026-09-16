1bdd34f847fc6a1682b6a237ce6dfbd92d30a00c
Ara <aram.airapetian@gmail.com>
2026-09-16 13:57:23 +0000

    feat: add persistent SQLite storage using PV and PVC

--- ansible/playbooks/roles/minikube-webapp/files/scripts/minikube.sh
+++ ansible/playbooks/roles/minikube-webapp/files/scripts/minikube.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start minikube with specified resources and addons
-minikube start --cpus=4 --memory=8192 --disk-size=20g --ports=80:80 --driver=docker --addons=ingress,metrics-server,dashboard
+minikube start --cpus=4 --memory=8192 --disk-size=20g --ports=80:80 --driver=docker --mount-string="/var/lib/sre-challenge:/mnt/sre-challenge" --addons=ingress,metrics-server,dashboard
